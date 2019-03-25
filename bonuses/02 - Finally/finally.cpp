9092edc8aa73f86aee0d87593b83a4aafc199cce
Teodor <ttoshkov@asteasolutions.com>
2019-03-25 02:05:35 +0200

    Bonus 2, possible solution

---
+++ bonuses/02 - Finally/finally.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using Procedure = void(*)();
+
+struct final_action {
+    Procedure proc;
+    ~final_action() { proc(); }
+};
+
+final_action finally(Procedure proc)
+{
+    return final_action{proc};
+}
+
+int main()
+{
+    final_action act = finally([](){std::cout << "Bye" << std::endl; });
+    std::cout << "Hi" << std::endl;
+    return 0;
+}
